a8c96121880fcbf550e24dad51d6893b249fe8e5
OscarJimenezz <114299099+OscarJimenezz@users.noreply.github.com>
2024-05-30 19:49:05 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -4,8 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
 from routes import *
 if __name__ == '__main__':
     app.run(port=os.getenv('PORT', 5000))
