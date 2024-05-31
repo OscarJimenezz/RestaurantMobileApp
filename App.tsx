20de41d2d93b8307bc6fba595dd78f675c003466
OscarJimenezz <114299099+OscarJimenezz@users.noreply.github.com>
2024-05-31 20:52:08 +0300

    add error catching

--- App.tsx
+++ App.tsx
@@ -4,16 +4,46 @@ import { createStackNavigator } from '@react-navigation/stack';
 import MenuScreen from './screens/MenuScreen';
 import ReservationsScreen from './screens/ReservationsScreen';
 import OrdersScreen from './screens/OrdersScreen';
+import { View, Text, Button } from 'react-native';
+
 const Stack = createStackNavigator();
+
+class ErrorBoundary extends React.Component<any, { hasError: boolean }> {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text>Something went wrong.</Text>
+          <Button title="Try Again" onPress={() => this.setState({ hasError: false })} />
+        </View>
+      );
+    }
+
+    return this.props.children; 
+  }
+}
+
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Menu">
-        <Stack.Screen name="Menu" component={MenuScreen} />
-        <Stack.Screen name="Reservations" component={ReservationsScreen} />
-        <Stack.Screen name="Orders" component={OrdersScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ErrorBoundary>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Menu">
+          <Stack.Screen name="Menu" component={MenuScreen} />
+          <Stack.Screen name="Reservations" component={ReservationsScreen} />
+          <Stack.Screen name="Orders" component={OrdersScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ErrorBoundary>
   );
 };
+
 export default App;
