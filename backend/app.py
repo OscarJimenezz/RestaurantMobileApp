5f00316653aa570e015fd6af41c1c46b9a20e1f0
OscarJimenezz <114299099+OscarJimenezz@users.noreply.github.com>
2024-05-31 21:29:40 +0300

    add readability

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,15 @@
 from flask import Flask
 import os
 from dotenv import load_dotenv
-load_dotenv()
-app = Flask(__name__)
-@app.route('/')
 from routes import *
+
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return home_route()
+
 if __name__ == '__main__':
-    app.run(port=os.getenv('PORT', 5000))
+    app.run(port=int(os.getenv('PORT', 5000)))
