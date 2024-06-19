203004966ee573123348ebfeed4f6bada2e373a1
OscarJimenezz <114299099+OscarJimenezz@users.noreply.github.com>
2024-06-19 18:23:39 +0300

    function added

--- components/MenuItem.tsx
+++ components/MenuItem.tsx
@@ -1,30 +1,45 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 
 interface MenuItemProps {
   name: string;
   description: string;
   price: number;
   category: string;
+  imageUrl?: string; // Optional imageUrl property
 }
 
-const MenuItem: React.FC<MenuItemProps> = ({name, description, price, category}) => {
+const MenuItem: React.FC<MenuItemProps> = ({name, description, price, category, imageUrl}) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.name}>{name}</Text>
-      <Text style={styles.description}>{description}</Text>
-      <Text style={styles.price}>{`$${price.toFixed(2)}`}</Text>
-      <Text style={styles.category}>{category}</Text>
+      {imageUrl && <Image source={{uri: imageUrl}} style={styles.image} />} {/* Display the image if imageUrl is provided */}
+      <View style={styles.textContainer}>
+        <Text style={styles.name}>{name}</Text>
+        <Text style={styles.description}>{description}</Text>
+        <Text style={styles.price}>{`$${price.toFixed(2)}`}</Text>
+        <Text style={styles.category}>{category}</Text>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: 'row', // Adjust layout to accommodate image
     padding: 20,
     marginBottom: 10,
     backgroundColor: '#f9f9f9',
     borderRadius: 8,
+    alignItems: 'center', // Align items in the center vertically
+  },
+  textContainer: {
+    flex: 1, // Take up remaining space after image
+  },
+  image: {
+    width: 60, // Set image width
+    height: 60, // Set image height
+    borderRadius: 30, // Make the image round
+    marginRight: 20, // Space between image and text
   },
   name: {
     fontSize: 18,
