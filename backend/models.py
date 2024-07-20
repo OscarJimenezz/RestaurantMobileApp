f719a40a2bb1ba63228dbcefd1cb0c7faaf96b48
OscarJimenezz <114299099+OscarJimenezz@users.noreply.github.com>
2024-07-20 10:34:12 +0300

    new function

--- backend/models.py
+++ backend/models.py
@@ -2,17 +2,27 @@ from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateT
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 import os
+from datetime import datetime
 
 DB_USER = os.getenv('DB_USER')
 DB_PASS = os.getenv('DB_PASS')
 DB_HOST = os.getenv('DB_HOST')
 DB_NAME = os.getenv('DB_NAME')
 
-DATABASE_URI = f'mysql+pymysql://{DB_DEF_USER}:{DB_DEF_PASS}@{DB_DEF_HOST}/{DB_NAME}'
+DATABASE_URI = f'mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}'
 
 engine = create_engine(DATABASE_URI, echo=True)
 
 Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(255), unique=True, nullable=False)
+    email = Column(String(255), unique=True, nullable=False)
+    hashed_password = Column(String(255), nullable=False)
+    reservations = relationship('Reservation', backref='user')
+    orders = relationship('Order', backref='user')
 
 class MenuItem(Base):
     __tablename__ = 'menu_items'
@@ -25,6 +35,7 @@ class MenuItem(Base):
 class Reservation(Base):
     __tablename__ = 'reservations'
     id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
     name = Column(String(255), nullable=False)
     email = Column(String(255))
     date_time = Column(DateTime, nullable=False)
@@ -32,8 +43,10 @@ class Reservation(Base):
 class Order(Base):
     __tablename__ = 'orders'
     id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
     quantity = Column(Integer, nullable=False)
     menu_item_id = Column(Integer, ForeignKey('menu_items.id'))
+    timestamp = Column(DateTime, index=True, default=datetime.utcnow)
     menu_item = relationship('MenuItem', back_populates='orders')
 
 Base.metadata.create_all(engine)
