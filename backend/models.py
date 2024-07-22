6eedc467c190ddf8063e282002be5a4978268c46
OscarJimenezz <114299099+OscarJimenezz@users.noreply.github.com>
2024-07-22 21:38:10 +0300

    change names

--- backend/models.py
+++ backend/models.py
@@ -4,17 +4,22 @@ from sqlalchemy.orm import relationship, sessionmaker
 import os
 from datetime import datetime
 
+# Environment Variables for Database Connection
 DB_USER = os.getenv('DB_USER')
 DB_PASS = os.getenv('DB_PASS')
 DB_HOST = os.getenv('DB_HOST')
 DB_NAME = os.getenv('DB_NAME')
 
+# Construct Database URI
 DATABASE_URI = f'mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}'
 
+# Initialize SQLAlchemy Engine
 engine = create_engine(DATABASE_URI, echo=True)
 
+# Base class for declarative models
 Base = declarative_base()
 
+# User Model
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
@@ -24,29 +29,33 @@ class User(Base):
     reservations = relationship('Reservation', backref='user')
     orders = relationship('Order', backref='user')
 
+# Menu Item Model
 class MenuItem(Base):
     __tablename__ = 'menu_items'
     id = Column(Integer, primary_key=True)
     name = Column(String(255), nullable=False)
     description = Column(String(255))
     price = Column(Float, nullable=False)
-    orders = relationship('Order', back_populates='menu_item')
+    related_orders = relationship('Order', back_populates='ordered_item')
 
+# Reservation Model
 class Reservation(Base):
     __tablename__ = 'reservations'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('users.id'))
-    name = Column(String(255), nullable=False)
-    email = Column(String(255))
-    date_time = Column(DateTime, nullable=False)
+    contact_name = Column(String(255), nullable=False)
+    contact_email = Column(String(255))
+    reservation_date_time = Column(DateTime, nullable=False)
 
+# Order Model
 class Order(Base):
     __tablename__ = 'orders'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('users.id'))
     quantity = Column(Integer, nullable=False)
     menu_item_id = Column(Integer, ForeignKey('menu_items.id'))
-    timestamp = Column(DateTime, index=True, default=datetime.utcnow)
-    menu_item = relationship('MenuItem', back_populates='orders')
+    order_timestamp = Column(DateTime, index=True, default=datetime.utcnow)
+    ordered_item = relationship('MenuItem', back_populates='related_orders')
 
+# Generate database schema
 Base.metadata.create_all(engine)
