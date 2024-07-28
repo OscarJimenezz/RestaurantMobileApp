516800071bfa1d0d112e04708cd5366c6d83ce54
OscarJimenezz <114299099+OscarJimenezz@users.noreply.github.com>
2024-07-28 08:12:52 +0300

    cleaning

--- backend/database.py
+++ backend/database.py
@@ -1,14 +1,25 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import os
 from dotenv import load_dotenv
 
+# Load environment variables
 load_dotenv()
+
+# Retrieve database URL from environment variables
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Create a SQLAlchemy engine instance
 engine = create_engine(DATABASE_URL)
+
+# Configure sessionmaker to be used for creating database sessions
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
+    """
+    Dependency that can be used to get a database session.
+    Yields a database session and ensures its closure after use.
+    """
     db = SessionLocal()
     try:
         yield db
